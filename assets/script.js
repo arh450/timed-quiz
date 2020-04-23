f881c278bd87f5878f1ef098aaa414e4e4b788fd
hank amadeus <hankamadeus@email.com>
2020-04-22 21:02:41 -0400

    redid array containing questions, how question is shown on html, onclick for answer buttons, and answer result on html page

--- assets/script.js
+++ assets/script.js
@@ -1,45 +1,67 @@
 var score = 0;
-var currentQuestion = 0;
-var seconds = 6;
+var seconds = 76;
+
 
 var questions = [
     {
         title: "Who is the current all-time homerun leader?",
-        answers: ["Babe Ruth", "Bud Abbott", "Barry Bonds", "Lou Costello"],
-        correct: 2
+        choiceA: "Babe Ruth",
+        choiceB: "Bud Abbott",
+        choiceC: "Barry Bonds",
+        choiceD: "Lou Costello",
+        correctAnswer: "c"
     },
     {
         title: "What team won the 1994 World Series?",
-        answers: ["New York Yankees", "Atlanta Braves", "Montreal Expos", "No Team Won"],
-        correct: 3
+        choiceA: "New York Yankees",
+        choiceB: "Atlanta Braves",
+        choiceC: "Montreal Expos",
+        choiceD: "No Team Won",
+        correctAnswer: "d"
     },
     {
-        title: "Who is the current all-time homerun leader?",
-        answers: ["Babe Ruth", "Bud Abbott", "Barry Bonds", "Lou Costello"],
-        correct: 2
+        title: "What player did Mr.Burns kick off The Springfield Nuclear Power Plant softball team for his sideburns?",
+        choiceA: "Jose Canseco",
+        choiceB: "Don Mattingly",
+        choiceC: "Steve Sax",
+        choiceD: "Wade Boggs",
+        correctAnswer: "b"
     },
     {
         title: "Which of these people played professional baseball and football simultaneously",
-        answers: ["David Wells", "Dion Sanders", "John Candy", "Bartolo Colon"],
-        correct: 1
+        choiceA: "David Wells",
+        choiceB: "Dion Sanders",
+        choiceC: "John Candy",
+        choiceD: "Bartolo Colon",
+        correctAnswer: "b"
     },
     {
         title: "What famous criminal was once a professional baseball player?",
-        answers: ["Lucky Luciano", "Al Capone", "John Dillinger", "Ted Bundy"],
-        correct: 2
-    },
+        choiceA: "John Dillinger",
+        choiceB: "Lucky Luciano",
+        choiceC: "Ted Bundy",
+        choiceD: "Al Capone",
+        correctAnswer: "a"
+    }
 ];
 
-$(document).ready(function () {
+var lastQuestionIndex = questions.length - 1;
+var runningQuestionIndex = 0;
+var correct;
 
-    $("#start-quiz-button").click(function (event) {
-        event.preventDefault();
-        $("#start-quiz-container").hide();
-        $("#during-quiz-container").show();
-        showQuestion();
-        startTimer();
-    });
-});
+
+function renderQuestion() {
+    var question = questions[runningQuestionIndex];
+    $("#question").text(question.title);
+    $("#a").text(question.choiceA);
+    $("#b").text(question.choiceB);
+    $("#c").text(question.choiceC);
+    $("#d").text(question.choiceD);
+    renderQuestion()
+    runningQuestionIndex++;
+
+}
+renderQuestion();
 
 function startTimer() {
     timerInterval = setInterval(function () {
@@ -57,18 +79,13 @@ function startTimer() {
 
 }
 
-function showQuestion() {
-    var question = questions[currentQuestion];
-    $("#question").text(question.title);
-    $("#answer-area").html("");
-    for (var i = 0; i < question.answers.length; i++) {
-        $("#answer-area").append(`<li id='${i}'><button>${question.answers[i]}<button></li>`);
+function checkAnswer(answer) {
+    correct = questions[runningQuestionIndex].correctAnswer;
+
+    if (answer === correct && runningQuestionIndex !== lastQuestionIndex) {
+        $("#answer-result").text("Correct!").css("color", "green");
+    } else {
+        $("#answer-result").text("Incorrect!").css("color", "red");
     }
+
 }
-    // function checkAnswer() {
-
-    // }
-
-    // function showFinalScore() {
-
-    // }
